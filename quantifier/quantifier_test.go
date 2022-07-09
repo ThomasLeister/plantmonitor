045f1d53c1660d959ce05a4265ae3e2b94d284b5
Thomas Leister <thomas.leister@mailbox.org>
2022-07-09 17:42:32 +0200

    Improve quantifier tests

--- quantifier/quantifier_test.go
+++ quantifier/quantifier_test.go
@@ -9,25 +9,38 @@ import (
 )
 
 // Thresholds according to example config:
-// 0  <low>  30 | 31  <normal>  66 | 67 <high> 100
+// 		0 <low> 30    ||    31 <normal> 66    ||    67 <high> 100
 
 const SENSOR_NORMALIZED_VALUE_LOW = 20
-const SENSOR_NORMALIZED_VALUE_LOW_EDGE_HIGHER = 30
+const SENSOR_NORMALIZED_VALUE_NORMAL_LOWEREDGE = 31
 const SENSOR_NORMALIZED_VALUE_NORMAL = 60
+const SENSOR_NORMALIZED_VALUE_NORMAL_UPPEREDGE = 66
+const SENSOR_NORMALIZED_VALUE_HIGH_LOWEREDGE = 67
 const SENSOR_NORMALIZED_VALUE_HIGH = 90
-const SENSOR_NORMALIZED_VALUE_HIGH_EDGE_LOWER = 67
+
+const HYSTERESIS_MARGIN = 2
+
+/*
+ * Test case
+ * Contains input value and expected outut values
+ * NOTE: Order of execution is important due to hysteresis
+ */
+type TestCase struct {
+	SensorNormalizedValue  int    // Input value for quantifier
+	ExpectedLevelDirection int    // Expected level direction
+	ExpectedLevelName      string // Expected level name
+}
 
 /* Global var for config*/
 var config configManagerPkg.Config
 
 /*
- *
- * TC-5 and TC-6: Test hysteresis by using edge values
+ * Test different sensor values and validate quantifier evaluation
  */
 func TestEvaluateValue(t *testing.T) {
 	var err error
-	var levelDirection int
-	var currentLevel QuantificationLevel
+	var testcases []TestCase
+	var previousSensorDir int
 
 	// Read config
 	config, err = configManagerPkg.ReadConfig("../config.yaml")
@@ -45,109 +58,64 @@ func TestEvaluateValue(t *testing.T) {
 	quantifier := Quantifier{}
 	quantifier.Init(&config, &sensor)
 
-	/*
-	 * Evaluate first value to create history
-	 */
-	sensor.Normalized.Current.Direction = 0
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_LOW)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
-	}
+	// Create test cases
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_LOW, 0, "low"})        // TC 0: Start with low level
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL, +1, "normal"}) // TC 1: Enter normal level
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_HIGH, +1, "high"})     // TC 2: Enter high level
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL, -1, "normal"}) // TC 3: Back to normal level
 
-	// Check if level is correct: We expect steady, because we don't have history.
-	if levelDirection != 0 {
-		t.Errorf("TC-1: Expected levelDirection == 0")
-	}
-	if currentLevel.Name != "low" {
-		t.Errorf("TC-1: Expected currentLevel == \"low\"")
-	}
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL, 0, "normal"})     // TC 4: Do not increase sensor value and expect same level as before.
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL + 1, 0, "normal"}) // TC 5: Slightly increase sensor value and expect same level as before.
 
-	/*
-	 * Evaluate second value for testing
-	 */
-	sensor.Normalized.Current.Direction = 1
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_NORMAL)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
-	}
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL_LOWEREDGE, 0, "normal"}) // TC 6: Test lower "normal" boundary. Should stay normal level.
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL_UPPEREDGE, 0, "normal"}) // TC 7: Test upper "normal" boundary. Should stay normal level.
 
-	// Check if level is correct: We expect rising
-	if levelDirection != +1 {
-		t.Errorf("TC-2: Expected levelDirection == +1")
-	}
-	if currentLevel.Name != "normal" {
-		t.Errorf("TC-2: Expected currentLevel == \"normal\"")
-	}
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL_LOWEREDGE - HYSTERESIS_MARGIN, 0, "normal"}) // TC 8: Check hysteresis margin downwards. Should keep level.
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL_UPPEREDGE + HYSTERESIS_MARGIN, 0, "normal"}) // TC 9: Check hysteresis margin upwards. Should keep level.
+
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_NORMAL_UPPEREDGE + HYSTERESIS_MARGIN + 1, 1, "high"})  // TC 10: Check hysteresis break-through: Should increase level to "high".
+	testcases = append(testcases, TestCase{SENSOR_NORMALIZED_VALUE_HIGH_LOWEREDGE - HYSTERESIS_MARGIN - 1, -1, "normal"}) // TC 11: Check hysteresis break-through: Should decrease level to "normal".
 
 	/*
-	 * Evaluate third value for testing
+	 * Test case loop: quantify and validate
+	 * Using the testcases
 	 */
-	sensor.Normalized.Current.Direction = 1
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_HIGH)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
+	for i, testcase := range testcases {
+		log.Printf("Running TC %d", i)
+
+		// Mock sensor value direction for hysteresis. This is usually set by Sensor.UpdateCurrentValue()
+		if testcase.SensorNormalizedValue > previousSensorDir {
+			sensor.Normalized.Current.Direction = +1
+		} else if testcase.SensorNormalizedValue < previousSensorDir {
+			sensor.Normalized.Current.Direction = -1
+		} else if testcase.SensorNormalizedValue == previousSensorDir {
+			sensor.Normalized.Current.Direction = 0
+		}
+		// Save current sensor value in history for next direction calculation
+		previousSensorDir = testcase.SensorNormalizedValue
+
+		// Evaluate testcase's sensor value
+		levelDirection, currentLevel, err := quantifier.EvaluateValue(testcase.SensorNormalizedValue)
+
+		// Check for eval errors
+		if err != nil {
+			t.Errorf("\tFailed to run testcase %d: %s", i, err)
+			continue
+		}
+
+		// Check level direction
+		if levelDirection != testcase.ExpectedLevelDirection {
+			t.Errorf("\tTestcase %d failed: Expected level direction: %d. Got level direction %d\n", i, testcase.ExpectedLevelDirection, levelDirection)
+			continue
+		}
+
+		// Check new level's name
+		if currentLevel.Name != testcase.ExpectedLevelName {
+			t.Errorf("\tTestcase %d failed: Expected level: %s. Got level %s\n", i, testcase.ExpectedLevelName, currentLevel.Name)
+			continue
+		}
+
+		log.Printf("\tTC %d successful!\n", i)
 	}
 
-	// Check if level is correct: We expect rising
-	if levelDirection != +1 {
-		t.Errorf("TC-3: Expected levelDirection == +1")
-	}
-	if currentLevel.Name != "high" {
-		t.Errorf("TC-3: Expected currentLevel == \"high\"")
-	}
-
-	/*
-	 * Evaluate fourth value for testing
-	 */
-	sensor.Normalized.Current.Direction = -1
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_NORMAL)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
-	}
-
-	// Check if level is correct: We expect rising
-	if levelDirection != -1 {
-		t.Errorf("TC-4: Expected levelDirection == -1")
-	}
-	if currentLevel.Name != "normal" {
-		t.Errorf("TC-4: Expected currentLevel == \"normal\"")
-	}
-
-	/*
-	 * Evaluate fifth value for testing
-	 * Input: low level highter edge value
-	 * Expected output: Should not (yet) reach "low" level due to Hysteresis (shift: sensor.Normalized.NoiseMargin | default: -2)
-	 */
-	sensor.Normalized.Current.Direction = -1
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_LOW_EDGE_HIGHER)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
-	}
-
-	// Check if level is correct: We expect rising
-	if levelDirection != 0 {
-		t.Errorf("TC-5: Expected levelDirection == 0")
-	}
-	if currentLevel.Name != "normal" {
-		t.Errorf("TC-5: Expected currentLevel == \"normal\"")
-	}
-
-	/*
-	 * Evaluate sixth value for testing
-	 * Input: high level lower edge value
-	 * Expected output: Should not (yet) reach "high" level due to Hysteresis (shift: sensor.Normalized.NoiseMargin | default: -2)
-	 */
-	sensor.Normalized.Current.Direction = +1
-	levelDirection, currentLevel, err = quantifier.EvaluateValue(SENSOR_NORMALIZED_VALUE_HIGH_EDGE_LOWER)
-	if err != nil {
-		log.Panic("Error happended during evaluation.")
-	}
-
-	// Check if level is correct: We expect rising
-	if levelDirection != 0 {
-		t.Errorf("TC-6: Expected levelDirection == 0")
-	}
-	if currentLevel.Name != "normal" {
-		t.Errorf("TC-6: Expected currentLevel == \"normal\"")
-	}
 }
